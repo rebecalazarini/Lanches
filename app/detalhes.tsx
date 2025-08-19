58c1d0bb7dbcf75305a77fb77ff2684a515e38a5
rebecalazarini <rebecalazarini2007@gmail.com>
2025-08-19 11:22:22 -0300

    alterações

--- app/detalhes.tsx
+++ app/detalhes.tsx
@@ -1,18 +1,43 @@
-import { router } from "expo-router";
 import { useLocalSearchParams } from "expo-router/build/hooks";
-import { Text, View, Button } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { posts } from "../assets/mockups/posts";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ecd3feff",
+  },
+    titulo: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#530e8fff",
+  },
+  text: {
+    fontSize: 14,
+    color: "#000000ff",
+  },
+  imagem: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+    marginVertical: 8,
+  },
+});
 
 export default function Index() {
-   const { nome } = useLocalSearchParams();
-  return (
+   const { id } = useLocalSearchParams();
+
+   let indice = posts.findIndex(post => post.id === Number(id));
+  
+   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+        style={styles.container}
     >
-      <Text>{nome}</Text>
+      <Text style={styles.titulo}>{posts[indice].title}</Text>
+      <Text style={styles.text}>{posts[indice].content}</Text>
+      <Image source={{ uri: posts[indice].image }} style={styles.imagem} />
     </View>
   );
 }
